c999558b4ff63f3e90cea4b1ad4a1181e4f5f797
Oliver Oayda <oaydaoliver@gmail.com>
2025-08-21 09:48:53 +1000

    testing dipole on custom masked flow

--- dipolesbi/scripts/masked_flow.py
+++ dipolesbi/scripts/masked_flow.py
@@ -12,6 +12,7 @@ from jax import random
 from matplotlib import pyplot as plt
 from corner import corner
 import healpy as hp
+from dipolesbi.tools.maps import SimpleDipoleMap
 
 from surjectors import (
     Chain,
@@ -28,6 +29,9 @@ from surjectors.util import (
     unstack,
 )
 
+from dipolesbi.tools.priors import DipolePrior
+from dipolesbi.tools.simulator import Simulator
+
 
 def make_model(dim, n_layers: int = 3, model="coupling"):
     def _bijector_fn(params):
@@ -37,6 +41,7 @@ def make_model(dim, n_layers: int = 3, model="coupling"):
     def _flow(method, **kwargs):
         layers = []
         order = jnp.arange(dim)
+        print(f'Using {n_layers} layers...')
         for i in range(n_layers):
             if model == "coupling":
                 print('Running masked coupling...')
@@ -127,42 +132,72 @@ def run(n_iter, model):
     # data = named_dataset(y, thetas)
 
     # healpix Poisson data
-    npix=12
-    ndim=npix
-    nbar=10.
+    NSIDE = 4
+    ndim=12 * NSIDE**2
+    MEAN_DENSITY=120_000.
+    OBSERVER_SPEED = 2.
+    DIPOLE_LONGITUDE = 215.
+    DIPOLE_LATITUDE = 40.
+    theta0 = jnp.repeat(
+        jnp.asarray(
+            [[MEAN_DENSITY, OBSERVER_SPEED, DIPOLE_LONGITUDE, DIPOLE_LATITUDE]]
+        ),
+        repeats=n,
+        axis=0
+    )
 
-    prior_samples = jnp.asarray(np.random.uniform(low=1, high=50, size=n))
-    y = jnp.asarray(
-        np.random.poisson(lam=prior_samples, size=(npix, n)),
-        dtype=jnp.float32
-    ).T
-    prior_samples = jnp.expand_dims(prior_samples, -1) # (n, 1)
-    data = named_dataset(y, prior_samples)
+    dipole = SimpleDipoleMap(NSIDE)    
+    mean_count_range = [0.95*MEAN_DENSITY, 1.05*MEAN_DENSITY]
+    prior = DipolePrior(mean_count_range=mean_count_range)
+    prior.change_kwarg('N', 'mean_density')
+    simulator = Simulator(prior, dipole.generate_dipole)
+    prior_samples, y = simulator.make_batch_simulations(
+        n_simulations=n,
+        n_workers=32,
+        simulation_batch_size=200
+    )
+    print(prior_samples)
+    # prior_samples = jnp.asarray(np.random.uniform(low=1, high=50, size=n))
+    # y = jnp.asarray(
+    #     np.random.poisson(lam=prior_samples, size=(npix, n)),
+    #     dtype=jnp.float32
+    # ).T
+    # prior_samples = jnp.expand_dims(prior_samples, -1) # (n, 1)
+    prior_samples = jnp.asarray(prior_samples)
+    y = jnp.asarray(y)
+    y_mean = y.mean(); y_std = y.std()
+    t_mean = prior_samples.mean(axis=0); t_std = prior_samples.std(axis=0)
+    normalise_y = lambda input: (input - y_mean) / y_std
+    normalise_t = lambda input: (input - t_mean) / t_std
+    unnormalise_y = lambda input: y_std * input + y_mean
+    unnormalise_t = lambda input: t_std * input + t_mean
 
-    model = make_model(ndim, model=model)
+    data = named_dataset(normalise_y(y), normalise_t(prior_samples))
+
+    model = make_model(ndim, model=model, n_layers=5)
     params, losses = train(hk.PRNGSequence(2), data, model, n_iter)
 
     # theta1 = jnp.full(n, -2.)
     # theta2 = jnp.full(n, -4.)
     # theta = jnp.stack([theta1, theta2], axis=1)
-    theta = jnp.full(n, nbar)[:, None] # (n, 1)
+    # theta = jnp.full(n, nbar)[:, None] # (n, 1)
     samples = model.apply(
         params,
         random.PRNGKey(2),
         method="sample",
-        x=theta
+        x=normalise_t(theta0)
     )
 
     plt.plot(losses)
     plt.show()
 
-    corner(np.asarray(samples), truths=12*[nbar]) # labels=['$D_1$', '$D_2$'])
-    plt.show()
+    # corner(np.asarray(samples), truths=ndim*[MEAN_DENSITY]) # labels=['$D_1$', '$D_2$'])
+    # plt.show()
     # plt.hist(samples[:, 0])
     # plt.hist(samples[:, 1])
     # plt.show()
 
-    return theta, y, model, samples
+    # return theta0, y, model, unnormalise_y(samples)
 
 
 if __name__ == "__main__":
@@ -170,5 +205,84 @@ if __name__ == "__main__":
     parser.add_argument("--n-iter", type=int, default=1_000)
     parser.add_argument("--model", type=str, default="coupling")
     args = parser.parse_args()
-    thetas, y, model, samples = run(args.n_iter, args.model)
+    # thetas, y, model, samples = run(args.n_iter, args.model)
+    n = 10_000
+
+    # 2D model with 2D data -> d_1 = 2*theta_1 + noise; d_2 = 2*theta_2 + noise
+    # noise ~ N([0, 0], sigma=[10, 10]), diagonal cov.
+    # ndim=2
+    # thetas = distrax.Normal(jnp.zeros(2), jnp.full(2, 10)).sample(
+    #     seed=random.PRNGKey(0), sample_shape=(n,)
+    # )
+    # y = 2 * thetas + distrax.Normal(jnp.zeros_like(thetas), 0.1).sample(
+    #     seed=random.PRNGKey(1)
+    # )
+    # data = named_dataset(y, thetas)
+
+    # healpix Poisson data
+    NSIDE = 4
+    ndim=12 * NSIDE**2
+    MEAN_DENSITY=12_000.
+    OBSERVER_SPEED = 2.
+    DIPOLE_LONGITUDE = 215.
+    DIPOLE_LATITUDE = 40.
+    theta0 = jnp.repeat(
+        jnp.asarray(
+            [[MEAN_DENSITY, OBSERVER_SPEED, DIPOLE_LONGITUDE, DIPOLE_LATITUDE]]
+        ),
+        repeats=n,
+        axis=0
+    )
+
+    dipole = SimpleDipoleMap(NSIDE)    
+    mean_count_range = [0.95*MEAN_DENSITY, 1.05*MEAN_DENSITY]
+    prior = DipolePrior(mean_count_range=mean_count_range)
+    prior.change_kwarg('N', 'mean_density')
+    simulator = Simulator(prior, dipole.generate_dipole)
+    prior_samples, y = simulator.make_batch_simulations(
+        n_simulations=n,
+        n_workers=32,
+        simulation_batch_size=200
+    )
+    print(prior_samples)
+    # prior_samples = jnp.asarray(np.random.uniform(low=1, high=50, size=n))
+    # y = jnp.asarray(
+    #     np.random.poisson(lam=prior_samples, size=(npix, n)),
+    #     dtype=jnp.float32
+    # ).T
+    # prior_samples = jnp.expand_dims(prior_samples, -1) # (n, 1)
+    prior_samples = jnp.asarray(prior_samples)
+    y = jnp.asarray(y)
+    y_mean = y.mean(axis=0); y_std = y.std(axis=0) + 1e-8
+
+    mean_nbar = prior_samples[:, 0].mean(); std_nbar = prior_samples[:, 0].std()
+    mean_v = prior_samples[:, 1].mean(); std_v = prior_samples[:, 1].std()
+    t_mean = jnp.asarray([mean_nbar, mean_v, 0, 0])
+    t_std = jnp.asarray([std_nbar, std_v, 1, 1])
+    # t_mean = prior_samples.mean(axis=0); t_std = prior_samples.std(axis=0)
+
+    normalise_y = lambda input: (input - y_mean) / y_std
+    unnormalise_y = lambda input: y_std * input + y_mean
+    normalise_t = lambda input: (input - t_mean) / t_std
+    unnormalise_t = lambda input: t_std * input + t_mean
+
+    data = named_dataset(normalise_y(y), normalise_t(prior_samples))
+
+    model = make_model(ndim, model=args.model, n_layers=5)
+    params, losses = train(hk.PRNGSequence(2), data, model, args.n_iter)
+
+    # theta1 = jnp.full(n, -2.)
+    # theta2 = jnp.full(n, -4.)
+    # theta = jnp.stack([theta1, theta2], axis=1)
+    # theta = jnp.full(n, nbar)[:, None] # (n, 1)
+    samples = model.apply(
+        params,
+        random.PRNGKey(2),
+        method="sample",
+        x=normalise_t(theta0)
+    )
+
+    plt.plot(losses)
+    plt.show()
+    mean_samples = unnormalise_y(samples).mean(axis=0)
 
