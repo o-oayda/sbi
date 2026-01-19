0cce60a7b0076dc7200608183389df7e662d4a80
Oliver Oayda <oaydaoliver@gmail.com>
2026-01-19 13:05:59 +1100

    update paper plots script

--- make_paper_plots.sh
+++ make_paper_plots.sh
@@ -8,7 +8,7 @@ python -m dipolesbi.scripts.evidence_acc --save_dir nside_64_coarse4_funnel_v2\
   --smoothing_window 2 --skip_delta --save_plot
 python -m dipolesbi.scripts.coverage_error_heatmap --mag-min 11 --vline-mag\
   --log-color --save-plot --figure-name cov_mag_error_heatmap.pdf
-./run_posterior_plots.sh fiducial_50k --round 14
+./run_posterior_plots.sh fiducial_50k_newerrs --round 14
 python -m dipolesbi.scripts.lnB_scatter
 
 # true vs NLE-inferred posterior
@@ -20,8 +20,8 @@ python -m dipolesbi.tools.posterior_cli\
 cp "corner_evidence_acc_example.pdf" ${fig_out_dir}
 
 # NPE vs NLE comparison for free gauss extra err
-python -m dipolesbi.tools.posterior_cli fiducial_50k_NPE/20251024_204611_SEED0_NPE\
-  fiducial_50k/20251011_155810_SEED0_NLE/\
+python -m dipolesbi.tools.posterior_cli ./fiducial_50k_newerrs_NPE/20260112_113740_SEED0_NPE\
+  ./fiducial_50k_newerrs/20260105_172601_SEED0_NLE\
   --corner "${fig_out_dir}corner_npe_vs_nle.pdf"\
   --corner-columns log10_n_initial_samples w1_extra_error observer_speed\
   dipole_longitude dipole_latitude\
